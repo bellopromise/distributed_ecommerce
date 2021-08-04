542550da9ff6436acbf67e6dc8b328ba2286d2af
promisebello <pbello@appzonegroup.com>
2021-08-04 15:43:47 +0100

    first commit

---
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AutoMapper;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using EventBusRabbitMQ.Common;
+using EventBusRabbitMQ.Events;
+using EventBusRabbitMQ.Producer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/v1/basket")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+
+
+        private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
+        private readonly EventBusRabbitMqProducer _eventBus;
+
+        public BasketController(IBasketRepository basketRepository, IMapper mapper, EventBusRabbitMqProducer eventBus)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBasket(string username)
+        {
+            BasketCart basket = await _basketRepository.GetBasket(username);
+            return Ok(basket);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateBasket([FromBody]BasketCart basketCart )
+        {
+            BasketCart basket = await _basketRepository.UpdateBasket(basketCart);
+            return Ok(basket);
+
+        }
+
+        [HttpDelete("{username}")]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            bool isSuccessful = await _basketRepository.DeleteBasket(username);
+            return Ok(new { Successful = isSuccessful });
+
+        }
+
+
+        [HttpPost("[action]")]
+        [ProducesResponseType( (int)HttpStatusCode.Accepted)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+
+        public async Task<IActionResult> Checkout(BasketCheckout checkout)
+        {
+            BasketCart basket = await _basketRepository.GetBasket(checkout.userName);
+
+            if (basket == null)
+            {
+                return BadRequest();
+            }
+
+
+            var basketRemoved = await _basketRepository.DeleteBasket(basket.UserName);
+            if(!basketRemoved)
+            {
+                return BadRequest();
+            }
+
+            //var eventMessage = _mapper.Map <BasketCheckoutEvent>(basket);
+            var eventMessage = new BasketCheckoutEvent { 
+            
+                AddressLine = checkout.AddressLine,
+                CardName = checkout.CardName,
+                CardNumber = checkout.CardNumber,
+                Country = checkout.Country,
+                CVV = checkout.CVV,
+                EmailAddress = checkout.EmailAddress,
+                Expiration = checkout.Expiration,
+                FirstName = checkout.FirstName,
+                LastName = checkout.LastName,
+                PaymentMethod = checkout.PaymentMethod,
+                State = checkout.State,
+                TotalPrice = checkout.TotalPrice,
+                userName = checkout.userName,
+                ZipCode = checkout.ZipCode
+            
+            };
+
+
+            eventMessage.RequestId = Guid.NewGuid();
+            eventMessage.TotalPrice = basket.TotalPrice;
+
+            try
+            {
+                _eventBus.PublishBasketCheckout(EventBusConstants.BasketCheckoutQueue, eventMessage);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest();
+
+            }
+
+            return Accepted(basket);
+
+        }
+
+
+
+    }
+}
---
+++ src/Ordering/Ordering.API/RabbitMQ/EventBusRabbitMQConsumer.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using EventBusRabbitMQ;
+using EventBusRabbitMQ.Common;
+using EventBusRabbitMQ.Events;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Ordering.Application.Commands;
+using Ordering.Application.Mapper;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.API.RabbitMQ
+{
+    public class EventBusRabbitMQConsumer
+    {
+
+
+        private readonly IRabbitMQConnection _connection;
+        private readonly IMapper _mapper;
+        private IServiceProvider _serviceProvider;
+
+        public EventBusRabbitMQConsumer(IServiceProvider serviceProvider, IRabbitMQConnection connection,
+            IMapper mapper)
+        {
+            _serviceProvider = serviceProvider;
+
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        //public CheckoutOrderHandler()
+        //{
+        //}
+
+
+        public void Consume()
+        {
+            var channel = _connection.CreateModel();
+            channel.QueueDeclare(queue: EventBusConstants.BasketCheckoutQueue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += ReceivedEvent;
+
+            channel.BasicConsume(queue: EventBusConstants.BasketCheckoutQueue, autoAck: false, consumer: consumer,noLocal:false,exclusive:false,arguments:null);
+
+
+        }
+
+        private async void ReceivedEvent(object sender, BasicDeliverEventArgs e)
+        {
+            if(e .RoutingKey == EventBusConstants.BasketCheckoutQueue)
+            {
+                var message = Encoding.UTF8.GetString(e.Body.Span);
+                var basketCheckoutEvent = JsonConvert.DeserializeObject<BasketCheckoutEvent>(message);
+
+                var command = _mapper.Map<CheckoutOrderCommand>(basketCheckoutEvent);
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var handler = scope.ServiceProvider.GetRequiredService<IMediator> ();
+                    await handler.Send(command);
+                }
+               
+            }
+        }
+
+
+
+        public void Disconnect()
+        {
+            _connection.Dispose();
+        }
+
+    }
+}
